c0dc83ed9e26950eab03ca88d3eba52cdc5b62bb
Milena-Knoll <milena-knoll@mail.ru>
2024-12-28 21:14:16 +0300

    fix(web): saga

--- src/store/customerReduser.js
+++ src/store/customerReduser.js
@@ -1,16 +1,22 @@
 export const FETCH_USERS = 'FETCH_USERS';
 export const SET_USERS = 'SET_USERS';
 
-const initialValue ={
-    users:[{ name: 'Антон' }]
-}
+const initialValue = {
+    users: [{ name: 'Антон' }],
+};
 
 export const customerReducer = (state = initialValue, action) => {
     switch (action.type) {
         case SET_USERS:
-            return {...state, users: [...state.users, ...action.payload]};       
+            return { ...state, users: [...state.users, ...action.payload] };
+
+        default:
+            return state;
     }
-    return state
 };
+
+// Экшен для установки пользователей
 export const setUsers = (payload) => ({ type: SET_USERS, payload });
-export const fetchUsers = () => ({ type: FETCH_USERS});
+
+// Экшен для запроса пользователей
+export const fetchUsers = () => ({ type: FETCH_USERS });
